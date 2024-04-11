6412ca7b451bfd1bee2a4d2efb87c1993fb17cbf
rama <ramastarzhu01@gmail.com>
2024-04-11 09:15:49 +0800

    fix: fixed the react component render error,fixed .babelrc configuration file

--- src/interview/webpack-basic/webpack@5.91/webpack.config.js
+++ src/interview/webpack-basic/webpack@5.91/webpack.config.js
@@ -1,4 +1,9 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+let pathsToClean = [
+    'dist',
+]
 
 module.exports = {
     mode: 'development',
@@ -7,31 +12,34 @@ module.exports = {
         path: __dirname + '/dist',
         filename: 'app.bundle.js'
     },
-    plugins: [new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html',
-        minify: {
-            collapseWhitespace: true,
-        },
-        hash: true,
-    })],
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            minify: {
+                collapseWhitespace: true,
+            },
+            hash: true,
+        }),
+        // new CleanWebpackPlugin(pathsToClean),
+    ],
     module: {
         rules: [
             {
                 test: /\.scss$/,
                 use: ['style-loader', 'css-loader', 'sass-loader']
             },
-            // { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-            // { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
-            {
-                test: /.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                options: {
-                    presets: ['react', 'es2015'],
-                    plugins: ['transform-class-plugins']
-                }
-            }
+            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+            // {
+            //     test: /.js$/,
+            //     loader: 'babel-loader',
+            //     exclude: /node_modules/,
+            //     options: {
+            //         presets: ['react', 'es2015'],
+            //         plugins: ['transform-class-plugins']
+            //     }
+            // }
         ]
     },
     devServer: {
