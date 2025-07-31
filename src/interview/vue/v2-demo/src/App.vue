164985b38ca60392f8cadb9a0fcf98d5a4ed3bb2
zhuxin <zhuxin@gzyltech.cn>
2025-07-31 10:35:37 +0800

    feat: 浏览器原生拖拽实现

---
+++ src/interview/vue/v2-demo/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+  {{foo}}
+  <button @click="handleClick">Click Me</button>
+  </div>
+</template>
+
+<script>
+import { fixed_obj } from './constant';
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      foo: Object.freeze(fixed_obj)
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.foo);
+      this.foo.a = 'Button was clicked!';
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
